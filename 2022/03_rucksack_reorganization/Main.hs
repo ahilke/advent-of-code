2b7a5be9a56264750de8ebd7860144794aa1e732
Arno Hilke <arno.hilke@tum.de>
2022-12-03 19:15:56 +0100

    Add Star 6

--- 2022/03_rucksack_reorganization/Main.hs
+++ 2022/03_rucksack_reorganization/Main.hs
@@ -4,18 +4,24 @@ import Constants (priority)
 import Paths_advent_of_code (getDataFileName)
 import Read (readLines)
 
-import Data.List (elemIndex, find)
-import Data.Maybe (fromJust, isJust)
+import Data.List (find)
+import Data.List.Split (chunksOf)
+import Data.Maybe (fromJust)
 
 findDuplicate :: [Char] -> [Char] -> Char
 findDuplicate a b = fromJust $ find searchChar a
   where
-    searchChar char = isJust $ elemIndex char b
+    searchChar char = char `elem` b
 
 splitList :: [a] -> ([a], [a])
 splitList list = splitAt half list
   where
     half = div (length list) 2
+
+findBadge :: [Char] -> [Char] -> [Char] -> Char
+findBadge a b c = fromJust $ find searchChar a
+  where
+    searchChar char = elem char b && elem char c
 
 main :: IO ()
 main = do
@@ -26,3 +32,9 @@ main = do
             mapper (a, b) = findDuplicate a b
     let priorities = map priority duplicates
     print $ sum priorities
+    let badgeGroups = chunksOf 3 input
+    let badges = map mapper badgeGroups
+          where
+            mapper (a:b:c:_) = findBadge a b c
+            mapper _ = error "error mapping badge groups to badges"
+    print $ sum $ map priority badges
