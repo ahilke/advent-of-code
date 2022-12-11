eb5e8d1b59cfeea05ce1c780d31e2ca742686185
Arno Hilke <arno.hilke@tum.de>
2022-12-12 00:16:35 +0100

    Add Star 20

--- 2022/10_cathode_ray_tube/Main.hs
+++ 2022/10_cathode_ray_tube/Main.hs
@@ -3,18 +3,29 @@ module Main where
 import Paths_advent_of_code (getDataFileName)
 import Read (readLines)
 
+import Data.Matrix (Matrix, fromLists, setElem, toLists)
 import Debug.Trace (trace)
+import Text.Printf
 
 data State =
     State
         { x :: Int
         , clockCycle :: Int
         , signalStrength :: Int
+        , screen :: Matrix Char
         }
-    deriving (Show)
+
+instance Show State where
+    show state =
+        printf "cycle: %d, x: %d, signal: %d" (clockCycle state) (x state) (signalStrength state) ++ "\n" ++ showScreen
+      where
+        showScreen = unlines $ toLists $ screen state
 
 isSignalStrengthCycle :: Int -> Bool
 isSignalStrengthCycle value = mod value 40 == 20
+
+processCycle :: State -> State
+processCycle = incrementCycle . draw
 
 incrementCycle :: State -> State
 incrementCycle state =
@@ -25,12 +36,29 @@ incrementCycle state =
                  }
         else state {clockCycle = clockCycle state + 1}
 
+draw :: State -> State
+draw state =
+    if spriteVisible
+        -- +1 for row/column, as matrices are 1-indexed
+        then state {screen = lightPixel (row + 1, column + 1) (screen state)}
+        else state
+  where
+    spriteVisible = abs (column - x state) <= 1
+    column = mod (clockCycle state - 1) 40 -- cycle starts at 1, but screen index at 0
+    row = div (clockCycle state - 1) 40 -- cycle starts at 1, but screen index at 0
+
+incrementRegister :: State -> Int -> State
+incrementRegister state value = state {x = x state + value}
+
+lightPixel :: (Int, Int) -> Matrix Char -> Matrix Char
+lightPixel = setElem '#'
+
 noop :: State -> State
-noop = incrementCycle
+noop = processCycle
 
 -- Note: Cycle needs to be incremented before changing `x`
 addX :: State -> Int -> State
-addX state value = (iterate incrementCycle state !! 2) {x = x state + value}
+addX state value = (iterate processCycle state !! 2) {x = x state + value}
 
 processLine :: State -> String -> State
 processLine state input
@@ -40,8 +68,13 @@ processLine state input
   where
     inputWords = trace (show state ++ " -> " ++ input) words input
 
+-- showState state = 
 main :: IO ()
 main = do
     input <- readLines (getDataFileName "2022/10_cathode_ray_tube/input.txt")
-    let endState = foldl processLine State {x = 1, clockCycle = 1, signalStrength = 0} input
+    let endState =
+            foldl
+                processLine
+                State {x = 1, clockCycle = 1, signalStrength = 0, screen = fromLists $ replicate 6 $ replicate 40 '.'}
+                input
     print endState
