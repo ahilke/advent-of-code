50593bbf85994b9cdb6b85f47326eb2b3b9dc350
Arno Hilke <arno.hilke@tum.de>
2024-12-02 22:35:24 +0100

    2024: day 2 part 2

--- 2024/src/main/kotlin/d02rednosedreports/Main.kt
+++ 2024/src/main/kotlin/d02rednosedreports/Main.kt
@@ -3,13 +3,25 @@ package d02rednosedreports
 import java.io.File
 
 fun main() {
-    val sum =
+    val readings =
         File("src/main/kotlin/d02rednosedreports/input.txt").readLines()
             .map { line -> line.split(Regex("""\s+""")) }
             .map { values -> values.map { value -> value.toInt() } }
-            .count { levels -> isSafelyIncreasing(levels) || isSafelyDecreasing(levels) }
 
-    println("Part 01: $sum") // 486
+    val strictSafeReadings = readings.count { isSafe(it) }
+
+    println("Part 01: $strictSafeReadings") // 2 / 486
+
+    val safeLevels =
+        readings
+            .map { levels -> levels.mapIndexed { index, level -> levels.toMutableList().apply { removeAt(index) }.toList() } }
+            .count { levelVariations -> levelVariations.any { isSafe(it) } }
+
+    println("Part 02: $safeLevels") // 4 / 540
+}
+
+fun isSafe(levels: List<Int>): Boolean {
+    return isSafelyIncreasing(levels) || isSafelyDecreasing(levels)
 }
 
 fun isSafelyIncreasing(levels: List<Int>): Boolean {
