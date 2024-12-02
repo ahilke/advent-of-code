731f308d3ce7409b7f8bb8553924fa629b918159
Arno Hilke <arno.hilke@tum.de>
2024-12-02 20:39:04 +0100

    2024: day 1 part 1

---
+++ 2024/src/main/kotlin/d01historianhysteria/Main.kt
@@ -0,0 +1,19 @@
+package d01historianhysteria
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val lines = File("src/main/kotlin/d01historianhysteria/input.txt").readLines()
+    val (leftList, rightList) =
+        lines.map {
+            it.split(Regex("""\s+"""), limit = 2).let { (left, right) ->
+                left to right
+            }
+        }.unzip()
+
+    val orderedPairs = leftList.map { it.toInt() }.sorted() zip rightList.map { it.toInt() }.sorted()
+    val sum = orderedPairs.fold(0) { sum, (left, right) -> sum + abs(left - right) }
+
+    println("Part 01: $sum")
+}
