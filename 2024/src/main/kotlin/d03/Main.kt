50f623e895dbc56219b991347d7b86b24c6055fc
Arno Hilke <arno.hilke@tum.de>
2024-12-05 02:29:32 +0100

    2024: day 3 part 2

--- 2024/src/main/kotlin/d03/Main.kt
+++ 2024/src/main/kotlin/d03/Main.kt
@@ -3,14 +3,31 @@ package d03
 import java.io.File
 
 fun main() {
-    val reading = File("src/main/kotlin/d03/input.txt").readText()
+    val code =
+        File("src/main/kotlin/d03/input.txt")
+            .readText()
+            .replace("\n", "")
 
-    val matches =
-        Regex("""mul\((\d+),(\d+)\)""")
-            .findAll(reading).sumOf { match ->
-                val (left, right) = match.destructured
-                left.toInt() * right.toInt()
-            }
+    val productSum = productSum(code)
 
-    println("Part 01: $matches") // 161 / 170068701
+    println("Part 01: $productSum") // 161 / 170068701
+
+    val enabledProductSum =
+        productSum(
+            code
+                // remove code between "don't()" and "do()"
+                .replace(Regex("""don't\(\).*?do\(\)"""), "")
+                // remove code after last "don't()"
+                .replace(Regex("""don't\(\)(?!.*do\(\)).*${"$"}"""), ""),
+        )
+
+    println("Part 02: $enabledProductSum") // 48 / 78683433
+}
+
+fun productSum(code: String): Int {
+    return Regex("""mul\((\d+),(\d+)\)""")
+        .findAll(code).sumOf { match ->
+            val (left, right) = match.destructured
+            left.toInt() * right.toInt()
+        }
 }
